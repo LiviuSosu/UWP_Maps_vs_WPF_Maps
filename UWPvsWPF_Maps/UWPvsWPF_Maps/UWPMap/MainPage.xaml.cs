08ad802f27960ec20bb9857140d7520df04826ab
LiviuSosu <sosuliviu@gmail.com>
2020-06-01 20:32:52 +0300

    Added the code.

---
+++ UWPvsWPF_Maps/UWPvsWPF_Maps/UWPMap/MainPage.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPMap
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            MapInit();
+
+            MapModes();
+
+            //SetLanguage();
+
+            AddPushPins();
+
+            AddMapPolygon();
+        }
+
+        void MapModes()
+        {
+            /*
+            link: https://docs.microsoft.com/en-us/windows/uwp/maps-and-location/display-maps
+           Use a map control to show rich and customizable map data in your app. A map control can display road maps, aerial, 3D, views, directions, search results, and traffic. 
+           On a map, you can display the user's location, directions, and points of interest. 
+           A map can also show aerial 3D views, Streetside views, traffic, transit, and local businesses.
+            */
+        }
+
+        void SetLanguage()
+        {
+            //https://stackoverflow.com/questions/42938755/display-language-for-mapcontrol-in-uwp?rq=1
+            throw new NotImplementedException("Not possible.");
+        }
+
+        void AddPushPins()
+        {
+            var MyLandmarks = new List<MapElement>();
+
+            BasicGeoposition snPosition = new BasicGeoposition { Latitude = 47.620, Longitude = -122.349 };
+            Geopoint snPoint = new Geopoint(snPosition);
+
+            var spaceNeedleIcon = new MapIcon
+            {
+                Location = snPoint,
+                NormalizedAnchorPoint = new Point(0.5, 1.0),
+                ZIndex = 0,
+                Title = "Pin 1"
+            };
+
+            spaceNeedleIcon.Tag = "Tag Pin 1";
+            MyLandmarks.Add(spaceNeedleIcon);
+
+            BasicGeoposition snPosition2 = new BasicGeoposition { Latitude = 41.622, Longitude = -122.340 };
+            Geopoint snPoint2 = new Geopoint(snPosition2);
+            var spaceNeedleIcon2 = new MapIcon
+            {
+                Location = snPoint2,
+                NormalizedAnchorPoint = new Point(0.5, 1.0),
+                ZIndex = 0,
+                Title = "Pin 3"
+            };
+
+            MyLandmarks.Add(spaceNeedleIcon2);
+
+            //
+            BasicGeoposition snPosition3 = new BasicGeoposition { Latitude = 47.620, Longitude = -117.549 };
+            Geopoint snPoint3 = new Geopoint(snPosition3);
+            var spaceNeedleIcon3 = new MapIcon
+            {
+                Location = snPoint3,
+                NormalizedAnchorPoint = new Point(0.5, 1.0),
+                ZIndex = 0,
+                Title = "Pin 2"
+            };
+
+            MyLandmarks.Add(spaceNeedleIcon3);
+
+
+            BasicGeoposition snPosition4 = new BasicGeoposition { Latitude = 41.622, Longitude = -117.549 };
+            Geopoint snPoint4 = new Geopoint(snPosition4);
+            var spaceNeedleIcon4 = new MapIcon
+            {
+                Location = snPoint4,
+                NormalizedAnchorPoint = new Point(0.5, 1.0),
+                ZIndex = 0,
+                Title = "Pin 4"
+            };
+
+            //MyLandmarks.Add(spaceNeedleIcon4);
+            //
+
+            var LandmarksLayer = new MapElementsLayer
+            {
+                ZIndex = 1,
+                MapElements = MyLandmarks
+            };
+
+            var MyLandmarks2 = new List<MapElement>();
+            MyLandmarks2.Add(spaceNeedleIcon4);
+            var LandmarksLayer2 = new MapElementsLayer
+            {
+                ZIndex = 1,
+                MapElements = MyLandmarks2
+            };
+
+            // MyLandmarks.Add(spaceNeedleIcon4);
+            LandmarksLayer.MapElementClick += MapElement_ClickEvent;
+            myMap.Layers.Add(LandmarksLayer);
+            LandmarksLayer2.MapElementClick += MapElement_ClickEvent2;
+            myMap.Layers.Add(LandmarksLayer2);
+        }
+
+        void AddMapPolygon()
+        {
+
+            var mapPolygon = new MapPolygon
+            {
+                Path = new Geopath(new List<BasicGeoposition> {
+                    new BasicGeoposition() {Latitude = 47.620, Longitude = -122.349 },
+                    new BasicGeoposition() {Latitude = 47.620, Longitude = -117.549 },
+                    new BasicGeoposition() {Latitude = 41.622, Longitude = -117.549 },
+                    new BasicGeoposition() {Latitude = 41.622, Longitude = -122.340},
+                }),
+                ZIndex = 1,
+                FillColor = Colors.Red,
+                StrokeColor = Colors.Blue,
+                StrokeThickness = 3,
+                StrokeDashed = false,
+
+            };
+
+            // Add MapPolygon to a layer on the map control.
+            var MyHighlights = new List<MapElement>();
+
+            MyHighlights.Add(mapPolygon);
+
+            var HighlightsLayer = new MapElementsLayer
+            {
+                ZIndex = 1,
+                MapElements = MyHighlights
+            };
+
+            myMap.Layers.Add(HighlightsLayer);
+        }
+
+        async void MapElement_ClickEvent(object sendeer, MapElementsLayerClickEventArgs e)
+        {
+            var dialog = new MessageDialog("Pushpin 1, 2 or 3 was clicked!");
+            await dialog.ShowAsync();
+        }
+
+        async void MapElement_ClickEvent2(object sendeer, MapElementsLayerClickEventArgs e)
+        {
+            var dialog = new MessageDialog("Pushpin 4 was clicked!");
+            await dialog.ShowAsync();
+        }
+
+        void MapInit()
+        {
+            BasicGeoposition snPosition = new BasicGeoposition { Latitude = 47.620, Longitude = -122.349 };
+            Geopoint snPoint = new Geopoint(snPosition);
+
+            myMap.Center = snPoint;
+            myMap.ZoomLevel = 6;
+        }
+    }
+}
---
+++ UWPvsWPF_Maps/UWPvsWPF_Maps/WpfMap/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfMap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MapInit();
+
+            MapModes();
+
+            SetLanguage();
+
+            AddPushPins();
+
+            AddMapPolygon();
+        }
+
+        void MapModes()
+        {
+            /*
+            link: https://docs.microsoft.com/en-us/previous-versions/bing/wpf-control/hh830432(v=msdn.10)
+            Available modes: Aerial, AerialWithLabels and Road.
+            */
+        }
+
+        void SetLanguage()
+        {
+            myMap.Culture = "en-en";
+        }
+
+        void AddPushPins()
+        {
+            Pushpin pushpin1 = new Pushpin();
+            pushpin1.Location = new Location(47.620, -122.349);
+            pushpin1.Content = "1";
+            pushpin1.MouseLeftButtonDown += MouseRightButtonDownEvent;
+            myMap.Children.Add(pushpin1);
+
+            Pushpin pushpin2 = new Pushpin();
+            pushpin2.Location = new Location(47.620, -117.549);
+            pushpin2.Content = "2";
+            pushpin2.MouseLeftButtonDown += MouseRightButtonDownEvent;
+            myMap.Children.Add(pushpin2);
+
+            Pushpin pushpin3 = new Pushpin();
+            pushpin3.Location = new Location(41.622, -117.549);
+            pushpin3.Content = "3";
+            pushpin3.MouseLeftButtonDown += MouseRightButtonDownEvent;
+            myMap.Children.Add(pushpin3);
+
+            Pushpin pushpin4 = new Pushpin();
+            pushpin4.Location = new Location(41.622, -122.340);
+            pushpin4.Content = "4";
+            pushpin4.MouseLeftButtonDown += MouseRightButtonDownEvent;
+            myMap.Children.Add(pushpin4);
+        }
+
+        void AddMapPolygon()
+        {
+            MapPolygon polygon = new MapPolygon();
+            polygon.Fill = new SolidColorBrush(Colors.Red);
+            polygon.Stroke = new SolidColorBrush(Colors.Blue);
+            polygon.StrokeThickness = 3;
+            polygon.Opacity = 0.5;
+
+            polygon.Locations = new LocationCollection() {
+            new Location(47.620, -122.349),
+            new Location(47.620,  -117.549),
+            new Location(41.622, -117.549 ),
+            new Location(41.622, -122.340)};
+
+            myMap.Children.Add(polygon);
+        }
+
+        void MouseRightButtonDownEvent(object sender, RoutedEventArgs e)
+        {
+            if (sender is Pushpin pushpin)
+            {
+                MessageBox.Show(string.Format("Pushpin {0} was clicked", pushpin.Content.ToString()));
+            }
+        }
+        void MapInit()
+        {
+            myMap.Center = new Location(47.620, -122.349);
+            myMap.ZoomLevel = 6;
+        }
+    }
+}
